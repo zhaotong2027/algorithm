6b7c63050d190032d7407e7786bc32d6c616f121
zhaotong2027 <18210192027@163.com>
2022-05-31 11:28:35 +0800

    No2linkedList-zhaotong-202205311128

---
+++ beginner/src/Class04/Code04_ReverseKGroupLinkedList.java
@@ -0,0 +1,89 @@
+package Class04;
+
+public class Code04_ReverseKGroupLinkedList {
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        int k = 3;
+
+        ListNode node = reverseKGroup(listNode1, k);
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+
+    }
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        ListNode start = head;
+        ListNode end = getKGroupEnd(start, k);
+        if (end == null) {
+            return head;
+        }
+        // 第一组
+        head = end;
+        reverse(start, end);
+        // 上组的尾节点
+        ListNode lastEnd = start;
+        while (lastEnd.next != null) {
+            start = lastEnd.next;
+            end = getKGroupEnd(start, k);
+            if (end == null) {
+                return head;
+            }
+            reverse(start, end);
+            lastEnd.next = end;
+            lastEnd = start;
+        }
+        return head;
+    }
+
+    public static void reverse(ListNode start, ListNode end) {
+        end = end.next;
+        ListNode pre = null;
+        ListNode cur = start;
+        ListNode next = null;
+        while (cur != end) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        start.next = end;
+    }
+
+    public static ListNode getKGroupEnd(ListNode start, int k) {
+        if (start.next == null) {
+            return start;
+        }
+        while (--k != 0 && start != null) {
+            start = start.next;
+        }
+        return start;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ beginner/src/Class04/Code05_AddLinkedList.java
@@ -0,0 +1,116 @@
+package Class04;
+
+public class Code05_AddLinkedList {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(0);
+//        ListNode list11 = new ListNode(3);
+//        ListNode list111 = new ListNode(6);
+//        list1.next = list11;
+//        list11.next = list111;
+        ListNode list2 = new ListNode(0);
+//        ListNode list22 = new ListNode(9);
+//        ListNode list222 = new ListNode(9);
+//        ListNode list2222 = new ListNode(9);
+//        list2.next = list22;
+//        list22.next = list222;
+//        list222.next = list2222;
+
+        ListNode node = addLinkedList(list1, list2);
+        while (list1 != null) {
+            System.out.print(list1.val);
+            list1 = list1.next;
+        }
+        System.out.print("+");
+        while (list2 != null) {
+            System.out.print(list2.val);
+            list2 = list2.next;
+        }
+        System.out.print("=");
+        while (node != null) {
+            System.out.print(node.val);
+            node = node.next;
+        }
+        System.out.println("");
+    }
+
+    public static ListNode addLinkedList(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode node = new ListNode();
+
+        // 个位数运算
+        int num = list1.val + list2.val;
+        node.val = num % 10;
+        if (num < 10 && (list1.next == null && list2.next == null)) {
+            return node;
+        }
+        ListNode reusltNode = node;
+        node.next = new ListNode(num / 10);
+        node = node.next;
+        list1 = list1.next;
+        list2 = list2.next;
+
+        // 十位及以上
+        while (list1 != null || list2 != null) {
+            if (list1 == null && list2 != null) {
+                num = list2.val + node.val;
+                node.val = num % 10;
+                if (list2.next != null) {
+                    node.next = new ListNode(num / 10);
+                }
+                if (num > 9) {
+                    node.next = new ListNode(num / 10);
+                }
+                node = node.next;
+                list2 = list2.next;
+            }
+
+            if (list1 != null && list2 == null) {
+                num = list1.val + node.val;
+                node.val = num % 10;
+                if (list1.next != null) {
+                    node.next = new ListNode(num / 10);
+                }
+                if (num > 9) {
+                    node.next = new ListNode(num / 10);
+                }
+                node = node.next;
+                list1 = list1.next;
+            }
+
+            if (list1 != null && list2 != null) {
+                num = list1.val + list2.val + node.val;
+                node.val = num % 10;
+                if (num > 9 || list1.next != null || list2.next != null) {
+                    node.next = new ListNode(num / 10);
+                }
+                node = node.next;
+                list1 = list1.next;
+                list2 = list2.next;
+            }
+
+        }
+        return reusltNode;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
