8586c03cd90cba9e9adca289e3ff05cf5c7973e8
zhaotong2027 <18210192027@163.com>
2022-06-14 19:54:09 +0800

    dichotomy-zhaotong-20220614

---
+++ leetcode/src/simple/Code0035_SearchInsert.java
@@ -0,0 +1,41 @@
+package src.simple;
+
+public class Code0035_SearchInsert {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 2;
+        System.out.println(searchInsert(nums, target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int L = 0;
+        int R = nums.length - 1;
+        while (L <= R) {
+            int M = L + ((R - L) >> 1);
+            if (nums[M] == target) {
+                return M;
+            } else if (nums[M] > target) {
+                R = M - 1;
+            } else {
+                L = M + 1;
+            }
+        }
+        return L;
+//        return process(nums, target, 0, nums.length-1);
+    }
+
+    // 数组中存在的可以找出来
+    public static int process(int[] nums, int target, int left, int right) {
+        while (left <= right) {  // 左标可能比右大，处于遍历到最大值依然比 target 小的情况
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {  // 开区间
+                right = mid - 1;
+            } else {  // 闭区间
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
---
+++ leetcode/src/simple/Code0069_MySqrt.java
@@ -0,0 +1,26 @@
+package src.simple;
+
+public class Code0069_MySqrt {
+    public static void main(String[] args) {
+        System.out.println(mySqrt(2147395599));
+    }
+    public static int mySqrt(int x) {
+        return (int)mySqrt((long)x);
+    }
+    public static long mySqrt(long x) {
+        long L = 1;
+        long R = x;
+        while (L <= R) {
+            long mid = L + (R - L) /2;
+            System.out.println(mid);
+            if (mid * mid == x) {
+                return mid;
+            } else if (mid * mid > x) {
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return L - 1;
+    }
+}
